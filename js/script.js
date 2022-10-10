3e459f0f6f00d570418998f3f3a66019ece9dac5
Vasyl <study.proger@gmail.com>
2022-10-10 11:25:46 +0200

    MenuCard with class component

--- js/script.js
+++ js/script.js
@@ -45,9 +45,8 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // якщо ми часто використовуємо event target то ми його можемо перевизначити в змінну
 
-
-// ------------------------------------------------------------
-/*
+  // ------------------------------------------------------------
+  /*
  Коли ми клікнули в визначений пункт меню ми повинні визнасити його номер в списку всіх табів 
  і по цьому номеру викликати функцію  showTabContent() . Ми використаємо метод перебору 
  Ми переберемо всі таби які лежать в змінній tabs - псевдомасив . Будемо порівнювати 
@@ -57,79 +56,132 @@ window.addEventListener("DOMContentLoaded", () => {
 
 */
 
-// 1. Нам потрібна функція котра буде приховувати не потрібні таби
-// 2. Показати потрібний таб
-// 3. Назначити обробник подій на таби (меню)
+  // 1. Нам потрібна функція котра буде приховувати не потрібні таби
+  // 2. Показати потрібний таб
+  // 3. Назначити обробник подій на таби (меню)
 
-//-----------------------------------------------------------------------------------------------
+  //-----------------------------------------------------------------------------------------------
 
+  // Робота із датами . Timer
 
-// Робота із датами . Timer 
+  // 1. У нас повинна бути функція яка буде встановлювати наш  таймер
+  // 2. Друга функція буде визначати різницю між датою коритстувача та дедлайном
+  // 3. Обновлення нашого таймера
 
-// 1. У нас повинна бути функція яка буде встановлювати наш  таймер 
-// 2. Друга функція буде визначати різницю між датою коритстувача та дедлайном 
-// 3. Обновлення нашого таймера 
-
-// Є два типи таймерів 
-/*
+  // Є два типи таймерів
+  /*
 
     Перший запускається коли користувач заходить на сайт і орієнтується тльки на нього
     Другий чесний таймер . Відраховує реальний дедлайн 
 
  */
 
+  const deadline = "2022-11-11";
 
-    const deadline = '2022-11-11';
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      seconds = Math.floor((t / 1000) % 60),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24);
 
-    function getTimeRemaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-            days = Math.floor( (t/(1000*60*60*24)) ),
-            seconds = Math.floor( (t/1000) % 60 ),
-            minutes = Math.floor( (t/1000/60) % 60 ),
-            hours = Math.floor( (t/(1000*60*60) % 24) );
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
 
-        return {
-            'total': t,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-        };
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return "0" + num;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock(".timer", deadline);
+
+  //
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.descr = descr;
+      this.title = title;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.changeToUAH();
     }
 
-    function getZero(num){
-        if (num >= 0 && num < 10) { 
-            return '0' + num;
-        } else {
-            return num;
-        }
+    changeToUAH() {
+      this.price = this.price * this.transfer;
     }
 
-    function setClock(selector, endtime) {
+    render() {
+      const element = document.createElement("div");
+      element.innerHTML = `
+        <div class="menu__item">
+        <img src= ${this.src} alt= ${this.alt}>
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>
+    </div>`;
 
-        const timer = document.querySelector(selector),
-            days = timer.querySelector("#days"),
-            hours = timer.querySelector('#hours'),
-            minutes = timer.querySelector('#minutes'),
-            seconds = timer.querySelector('#seconds'),
-            timeInterval = setInterval(updateClock, 1000);
+    this.parent.append(element);
 
-        updateClock();
-
-        function updateClock() {
-            const t = getTimeRemaining(endtime);
-
-            days.innerHTML = getZero(t.days);
-            hours.innerHTML = getZero(t.hours);
-            minutes.innerHTML = getZero(t.minutes);
-            seconds.innerHTML = getZero(t.seconds);
-
-            if (t.total <= 0) {
-                clearInterval(timeInterval);
-            }
-        }
     }
+  }
 
-    setClock('.timer', deadline);
+//   new MenuCard(
+//     "img/tabs/vegy.jpg",
+//     "vegy",
+//     'Меню "Фитнес"',
+//     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+//     9,
+//     ".menu .container"
+// ).render();
+
+
+new MenuCard(
+  "img/tabs/vegy.jpg",
+  "vegy",
+  'Меню "Фитнес"',
+  'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+  9,
+  ".menu .container"
+).render();
 
 });
